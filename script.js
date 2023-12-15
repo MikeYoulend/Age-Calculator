c02f582242a893cecd843161cf6f7bfebdcf9a2b
MikeYoulend <mikeyoulend@gmail.com>
2023-12-15 14:19:24 +0100

    Age Calculator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0]; //per non andare oltre nella scelta della data
+let result = document.getElementById("result");
 
 function calculateAge() {
 	let birthDate = new Date(userInput.value);
@@ -28,7 +29,15 @@ function calculateAge() {
 	if (d2 >= d1) {
 		d3 = d2 - d1;
 	} else {
+		m3--;
+		d3 = getDaysInMonth(y1, m1) + d2 - d1;
 	}
+
+	if (m3 < 0) {
+		m3 = 11;
+		y3--;
+	}
+	console.log(y3, m3, d3);
 }
 
 function getDaysInMonth(year, month) {
